bd0b1c8af2a047d19a5f8036742969e049545c5c
Bhavik27 <bhavik27071999@gmail.com>
2021-03-09 22:20:04 +0530

    authentication bearer added

--- WebUI/src/app/services/api.service.ts
+++ WebUI/src/app/services/api.service.ts
@@ -2,9 +2,10 @@ import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http'
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { CoreService } from './core.service';
 
 
-const headers = new HttpHeaders()
+let headers = new HttpHeaders()
   .set('Content-Type', 'application/json')
   .set('Accept', 'application/json');
 
@@ -13,17 +14,30 @@ const headers = new HttpHeaders()
 })
 export class ApiService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    private coreService: CoreService) { }
 
   get(url: string): Observable<any> {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'bearer ' + this.coreService.Token);
     return this.httpClient.get(environment.url + url, { headers: headers })
   }
 
   post(url: string, model: any): Observable<any> {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'bearer ' + this.coreService.Token);
     return this.httpClient.post(environment.url + url, model, { headers: headers })
   }
 
   delete(url: string): Observable<any> {
+    const headers = new HttpHeaders()
+      .set('Content-Type', 'application/json')
+      .set('Accept', 'application/json')
+      .set('Authorization', 'bearer ' + this.coreService.Token);
     return this.httpClient.delete(environment.url + url, { headers: headers })
   }
 
